34ac868eaaaf96e372712a2ec56e074a1bc87ed2
caganozsir <caganozsir@gmail.com>
2024-07-29 16:21:38 +0300

    afjfakdsgj

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className={styles.header}>
         <h1 style={{color:"white"}}>To-Do List</h1>
-        <h1 style={{color:"white"}}>Çağan Özsır</h1>
+        <h2 style={{color:"white"}}>Çağan Özsır</h2>
     </div>
   )
 }
